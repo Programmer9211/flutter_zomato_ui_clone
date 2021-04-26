4bba69d0f31616641e8c1691a2844cb92e7f4aa9
Programmer9211 <adityachaudhary1029@gmail.com>
2021-04-26 16:03:43 +0530

    Menu Screen

--- lib/Screens/SubScreen/OrderScreen.dart
+++ lib/Screens/SubScreen/OrderScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zomato_clone/Screens/SubScreen/MenuScreen.dart';
 import 'package:zomato_clone/const.dart';
 
 class OrderScreen extends StatelessWidget {
@@ -72,79 +73,83 @@ class OrderScreen extends StatelessWidget {
       child: ListView.builder(
           itemCount: restaurantList.length,
           itemBuilder: (context, index) {
-            return itemBuilder(size, index);
+            return itemBuilder(size, index, context);
           }),
     );
   }
 
-  Widget itemBuilder(Size size, int index) {
+  Widget itemBuilder(Size size, int index, BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 18.0),
-      child: Material(
-        elevation: 3,
-        borderRadius: BorderRadius.circular(18),
-        child: Container(
-          height: size.height / 2.5,
-          width: size.width / 1.1,
-          child: Column(
-            children: [
-              Container(
-                height: size.height / 4,
-                width: size.width / 1.1,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(18),
-                    topRight: Radius.circular(18),
-                  ),
-                  image: DecorationImage(
-                      image: NetworkImage(restaurantList[index].imageUrl),
-                      fit: BoxFit.cover),
-                ),
-              ),
-              Container(
-                height: size.height / 12,
-                width: size.width / 1.2,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      restaurantList[index].title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
+      child: GestureDetector(
+        onTap: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => MenuScreen())),
+        child: Material(
+          elevation: 3,
+          borderRadius: BorderRadius.circular(18),
+          child: Container(
+            height: size.height / 2.5,
+            width: size.width / 1.1,
+            child: Column(
+              children: [
+                Container(
+                  height: size.height / 4,
+                  width: size.width / 1.1,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(18),
+                      topRight: Radius.circular(18),
                     ),
-                    Container(
-                      height: size.height / 25,
-                      width: size.width / 7,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.green,
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(
-                        restaurantList[index].rating,
+                    image: DecorationImage(
+                        image: NetworkImage(restaurantList[index].imageUrl),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Container(
+                  height: size.height / 12,
+                  width: size.width / 1.2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        restaurantList[index].title,
                         style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: size.width / 1.2,
-                child: Text(
-                  "${restaurantList[index].locations}   \t\t\t\t\t\t\t\t\t\t\t\t\t\t  ${restaurantList[index].price} for one",
-                  style: TextStyle(
-                    fontSize: 12.9,
-                    fontWeight: FontWeight.w500,
+                      Container(
+                        height: size.height / 25,
+                        width: size.width / 7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.green,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          restaurantList[index].rating,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  width: size.width / 1.2,
+                  child: Text(
+                    "${restaurantList[index].locations}   \t\t\t\t\t\t\t\t\t\t\t\t\t\t  ${restaurantList[index].price} for one",
+                    style: TextStyle(
+                      fontSize: 12.9,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/const.dart
+++ lib/const.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class FoodItems {
   String name, imageUrl;
 
